09f91d739291e77b4d40f676a37fb75b7ac65aca
yimu <yimu@example.com>
2026-03-10 14:36:02 +0800

    修改ST-SVimTD3网络结构

--- algorithm/state_adapter.py
+++ algorithm/state_adapter.py
@@ -6,6 +6,7 @@ class StateAdapter(nn.Module):
 
     def __init__(self, input_dim: int, output_dim: int):
         super().__init__()
+        self.input_norm = nn.LayerNorm(input_dim)
         self.net = nn.Sequential(
             nn.Linear(input_dim, output_dim),
             nn.LayerNorm(output_dim),
@@ -13,4 +14,5 @@ class StateAdapter(nn.Module):
         )
 
     def forward(self, x):
+        x = self.input_norm(x)
         return self.net(x)
