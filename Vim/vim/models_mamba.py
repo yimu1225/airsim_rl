4d4d3d89169dc88d0875cb1b888609297e32fba2
yimu <yimu1225@163.com>
2026-07-29 11:32:18 +0800

    fix: install bidirectional Vision Mamba dependencies

--- Vim/vim/models_mamba.py
+++ Vim/vim/models_mamba.py
@@ -19,8 +19,6 @@ import inspect
 from collections import namedtuple
 
 from mamba_ssm.modules.mamba_simple import Mamba
-from mamba_ssm.utils.generation import GenerationMixin
-from mamba_ssm.utils.hf import load_config_hf, load_state_dict_hf
 
 from .rope import *
 import random
